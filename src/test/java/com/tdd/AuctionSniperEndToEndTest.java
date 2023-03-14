9c6f8531d9cb9f36990eaf0524e92ed0a50169e2
Daniel Macrinici <macrinici.d@gmail.com>
2023-03-14 12:39:33 +0000

    update end to end test and add README

---
+++ src/test/java/com/tdd/AuctionSniperEndToEndTest.java
@@ -0,0 +1,18 @@
+package com.tdd;
+
+import org.junit.jupiter.api.Test;
+
+// TODO: build end-to-end test infrastructure
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem(); // Step 1
+        application.startBiddingIn(auction); // Step 2
+        auction.hasReceivedJoinRequestFromSniper(); // Step 3
+        auction.announceClosed(); // Step 4
+        application.showsSniperHasLostAuction(); // Step 5
+    }
+}
