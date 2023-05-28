beb4f97a071b3ed118cf939aae12e2cb746bf20a
Daniel Macrinici <macrinici.d@gmail.com>
2023-05-28 17:15:59 +0200

    add connection from the Sniper(OpenFire Client) to the Auction (Main App)

--- src/main/java/com/tdd/App.java
+++ src/main/java/com/tdd/App.java
@@ -7,6 +7,13 @@ import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.LineBorder;
 
+import org.hamcrest.text.X;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
 /**
  * Hello world!
  */
@@ -17,6 +24,14 @@ public final class App {
     }
 
     private MainWindow ui;
+    private static final int ARG_HOSTNAME = 0;
+    private static final int ARG_USERNAME = 1;
+    private static final int ARG_PASSWORD = 2;
+    private static final int ARG_ITEM_ID = 3;
+
+    public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+    public static final String AUCTION_RESOURCE = "Auction";
+    public static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
 
     public static String SNIPER_STATUS_NAME = "SNIPER STATUS";
     public static String STATUS_JOINING = "JOINING";
@@ -29,6 +44,26 @@ public final class App {
     public static void main(String... args) throws Exception {
         App app = new App();
         System.out.println("Running Application...");
+        XMPPConnection connection = connection(args[ARG_HOSTNAME], args[ARG_USERNAME], args[ARG_PASSWORD]);
+        Chat chat = connection.getChatManager().createChat(auctionId(args[ARG_ITEM_ID], connection),
+        new MessageListener() {
+            public void processMessage(Chat aChat, Message message) {
+                // nothing yet
+            }
+        });
+        chat.sendMessage(new Message());
+    }
+
+    private static XMPPConnection connection(String hostname, String username, String password) throws XMPPException {
+        XMPPConnection connection = new XMPPConnection(hostname);
+        connection.connect();
+        connection.login(username, password, AUCTION_RESOURCE);
+
+        return connection;
+    }
+
+    private static String auctionId(String itemId, XMPPConnection connection) {
+        return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
     }
 
     private void startUserInterface() throws Exception {
